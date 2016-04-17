7f9326eada13da4317d8a083467a1196732e0f7d
berkas1 <berka@berkasimon.com>
2016-04-18 00:42:41 +0200

    improvements & add readme
    Signed-off-by: berkas1 <berka@berkasimon.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+## gitlab-runner-deploy
+
+Simple script for automated gitlab-runner deployment.
+Currently working with Vagrant and using Docker as executor.
+
+
+###How to use
+Clone and edit config variables in deploy.sh
+
+```
+git clone https://github.com/berkas1/gitlab-runner-deploy.git
+cd gitlab-runner-deploy
+
+# edit config variables
+
+vagrant up
+```
--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,67 @@
 # deploy github runner based on docker
 # tested on Debian 8.3 x64
 # tested on digitalocean and with Vagrant
+# v0.1
 
 
-_ci_server='https://git.hosting.berkasimon.com:1001'
-_ci_token='Kf1G2gbtWrRrr3vHNzig'
+# CONFIGURATION - edit before deploy
+# if your gitlab web interface uses self-signed certificate, provide its url 
+_ci_selfsigned_crt=''
+# 'hostname' of your github server (eextracted from URL) 
+_ci_selfsigned_hostname=''
+
+
+
+
+# URL of Gitlab CI, like 'https://gitlab.com/ci'
+_ci_server=''
+# Runner token
+_ci_token=''
+# Runner description
+_ci_desc='ci-vagrant'
+# Runner tag(s)
 _ci_tags='ci-vagrant'
+
+
 _ci_executor='docker'
 _ci_executor_image='ruby:2.1'
 
-sudo apt-get install vim aptitude htop curl
-sudo aptitude update
-sudo aptitude upgrade
-sudo curl -sSL https://get.docker.com/ | sh
-sudo curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh >s.sh
-sudo bash s.sh
-sudo aptitude install gitlab-ci-multi-runner
 
-printf $_ci_server$'\n'$_ci_token$'\n'_ci_tags$'\n'$_ci_executor$'\n'$_ci_executor_image$'\n'
+
+
+
+
+
+
+# DO NOT EDIT
+
+echoInfo() {
+    echo;echo;
+    echo $1;
+    echo;
+}
+
+
+
+sudo apt-get install -y vim aptitude htop curl > /dev/null
+echoInfo "Updating system"
+sudo apt-get update > /dev/null
+sudo apt-get -y upgrade > /dev/null
+echoInfo "Installing docker"
+curl -sSL https://get.docker.com/ | sudo sh > /dev/null && echo "Docker installed"
+curl -s -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh > s.sh
+echoInfo "Preparing environment for Gitlab runner"
+sudo bash s.sh > /dev/null
+echoInfo "Installing multi-runner"
+sudo apt-get -y install gitlab-ci-multi-runner > /dev/null
+
+
+if [[ -n "$_ci_selfsigned_crt" ]] ; then
+    sudo mkdir /etc/gitlab-runner/certs
+    curl -s $_ci_selfsigned_crt | sudo tee -a /etc/gitlab-runner/certs/$_ci_selfsigned_hostname.crt > /dev/null
+fi
+
+
+
+printf $_ci_server$'\n'$_ci_token$'\n'$_ci_desc$'\n'$_ci_tags$'\n'$_ci_executor$'\n'$_ci_executor_image$'\n' | sudo gitlab-ci-multi-runner register && \
+    echoInfo "Gitlab runner installed successfully."
